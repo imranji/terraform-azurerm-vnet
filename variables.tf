df51b51e14516a0abdab2c496cce15e75866357b
imran <imran@imranstudentoutlook.onmicrosoft.com>
2024-08-19 20:29:51 +0530

    TF Module

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "vnet_name" {
+  description = "The name of the virtual network"
+  type        = string
+}
+
+variable "address_space" {
+  description = "The address space for the virtual network"
+  type        = list(string)
+}
+
+variable "location" {
+  description = "The Azure region to deploy resources in"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "subnets" {
+  description = "A list of subnets to create"
+  type = list(object({
+    name           = string
+    address_prefix = string
+  }))
+}
